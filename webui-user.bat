1320dc666c2a0a68767fcc77a890d445229d38db
yiyiooo <yiyiooo@outlook.com>
2022-11-24 13:08:51 +0800

    update

--- webui-user.bat
+++ webui-user.bat
@@ -1,9 +1,11 @@
 @echo off
 
-set PYTHON=
+set PYTHON=venv\Scripts\python.exe
 set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+set VENV_DIR=venv
+set COMMANDLINE_ARGS= --medvram  --always-batch-cond-uncond --xformers --deepdanbooru  --no-half-vae --api --listen
+@REM --medvram  --always-batch-cond-uncond --xformers --deepdanbooru  --no-half-vae --api --listen
+set MODEL_PATH=model.animefull-final-pruned.ckpt
 set ACCELERATE=
 
 call webui.bat
