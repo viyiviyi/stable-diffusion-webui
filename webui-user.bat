0a44fa35be0ab2c9bdc59a32d1fe8f36c9899da3
viyiviyi <viyiviyi@outlook.com>
2023-02-05 09:27:09 +0800

    启动命令更新

--- webui-user.bat
+++ webui-user.bat
@@ -3,8 +3,8 @@
 set PYTHON=venv\Scripts\python.exe
 set GIT=
 set VENV_DIR=venv
-set COMMANDLINE_ARGS= --disable-safe-unpickle --medvram --deepdanbooru --xformers --api --listen --no-half-vae --disable-console-progressbars --port=8210
-@REM --medvram  --always-batch-cond-uncond --xformers --deepdanbooru  --no-half-vae --api --listen
+set COMMANDLINE_ARGS= --disable-safe-unpickle --port=8210 --medvram --deepdanbooru --xformers --api --listen --no-half-vae --disable-console-progressbars
+@REM --medvram  --always-batch-cond-uncond --xformers --deepdanbooru  --no-half-vae --api --listen  --reinstall-xformers --reinstall-torch
 set MODEL_PATH=model.animefull-final-pruned.ckpt
 set ACCELERATE=
 
