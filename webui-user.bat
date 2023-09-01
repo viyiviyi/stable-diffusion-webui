45fb5c98fddb7e8d628ad13612dc9ea6865fa10d
viyiviyi <viyiviyi@outlook.com>
2023-09-01 11:17:32 +0800

    update

--- webui-user.bat
+++ webui-user.bat
@@ -1,11 +1,8 @@
 @echo off
 
-set PYTHON=venv\Scripts\python.exe
+set PYTHON=
 set GIT=
-set VENV_DIR=venv
-set COMMANDLINE_ARGS= --disable-safe-unpickle --port=8210 --medvram --deepdanbooru --xformers --api --listen --no-half-vae --disable-console-progressbars
-@REM --medvram  --always-batch-cond-uncond --xformers --deepdanbooru  --no-half-vae --api --listen  --reinstall-xformers --reinstall-torch
-set MODEL_PATH=model.animefull-final-pruned.ckpt
-set ACCELERATE=
+set VENV_DIR=
+set COMMANDLINE_ARGS=
 
 call webui.bat
