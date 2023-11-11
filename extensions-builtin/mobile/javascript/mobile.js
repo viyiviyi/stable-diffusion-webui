d344795980ac5bc579cccf05e9b8b47c3147c754
viyiviyi <viyiviyi@outlook.com>
2023-11-11 14:26:18 +0800

    修复有时候页面不能正常调换输入和输出面板的问题

--- extensions-builtin/mobile/javascript/mobile.js
+++ extensions-builtin/mobile/javascript/mobile.js
@@ -1,36 +1,19 @@
-var isSetupForMobile = false;
-
-function isMobile() {
-    return true
-    for (var tab of ["txt2img", "img2img"]) {
-        var imageTab = gradioApp().getElementById(tab + '_results');
-        if (imageTab && imageTab.offsetParent && imageTab.offsetLeft == 0) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
 function reportWindowSize() {
-    var currentlyMobile = isMobile();
-    if (currentlyMobile == isSetupForMobile) return;
-    isSetupForMobile = currentlyMobile;
+  for (var tab of ["txt2img", "img2img"]) {
+    var setting = gradioApp().getElementById(tab + "_settings");
+    if (!setting) return;
+    var result = gradioApp().getElementById(tab + "_results");
+    var target = setting.parentElement;
+    if (target.firstElementChild == result) return;
+    var resizeHandle = target.getElementsByClassName("resize-handle")[0];
 
-    for (var tab of ["txt2img", "img2img"]) {
-        var result = gradioApp().getElementById(tab + '_results');
-        var target = gradioApp().getElementById(tab + '_settings').parentElement;
-        var resizeHandle = target.getElementsByClassName('resize-handle')[0];
-        
-        target.insertBefore(resizeHandle, target.firstElementChild);
-        target.insertBefore(result, target.firstElementChild);
-
-        gradioApp().getElementById(tab + '_results').classList.toggle('mobile', currentlyMobile);
-    }
+    target.insertBefore(resizeHandle, target.firstElementChild);
+    target.insertBefore(result, target.firstElementChild);
+  }
 }
 
 window.addEventListener("resize", reportWindowSize);
 
-onUiLoaded(function() {
-    reportWindowSize();
+onUiLoaded(function () {
+  reportWindowSize();
 });
