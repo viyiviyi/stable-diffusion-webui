20cd277d216df9c218a263045852a0098af1536c
viyiviyi <viyiviyi@outlook.com>
2024-03-02 16:54:30 +0800

    更方便使用的移动端样式和布局调整

--- extensions-builtin/mobile/javascript/mobile.js
+++ extensions-builtin/mobile/javascript/mobile.js
@@ -27,6 +27,20 @@ function reportWindowSize() {
     }
 }
 
+function reportWindowSize() {
+    for (var tab of ["txt2img", "img2img"]) {
+        var setting = gradioApp().getElementById(tab + "_settings");
+        if (!setting) return;
+        var result = gradioApp().getElementById(tab + "_results");
+        var target = setting.parentElement;
+        if (target.firstElementChild == result) return;
+        var resizeHandle = target.getElementsByClassName("resize-handle")[0];
+
+        target.insertBefore(resizeHandle, target.firstElementChild);
+        target.insertBefore(result, target.firstElementChild);
+    }
+}
+
 window.addEventListener("resize", reportWindowSize);
 
 onUiLoaded(function() {
